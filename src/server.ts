0b2bd869d6773f992831215095047942d4232752
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2022-12-11 22:40:28 -0500

    Continue...

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,71 @@
 import express from 'express'
+import mariadb from 'mariadb'
 const app = express()
 
 app.get('/healthcheck', (req, res) => {
-    res.json({
-        status: 'OK',
-        message: 'Service is OK'
-    })
+    async function asyncFunction() {
+        let conn
+        try {
+            conn = await mariadb.createConnection({
+                host: process.env.MYSQL_HOST || 'localhost',
+                user: process.env.MYSQL_USER || 'user',
+                password: process.env.MYSQL_PASSWORD || 'secret',
+                database: process.env.MYSQL_DATABASE || 'database'
+            });
+
+            await conn.query('select 1', [2]);
+            res.json({
+                status: 'OK',
+                message: 'Service is OK'
+            })
+        } catch (err) {
+            res.status(500).json({
+                status: 'NOT_OK',
+                message: 'Service is NOT_OK. Connection to database failed'
+            })
+        }
+        finally {
+            conn?.end();
+        }
+    }
+
+    asyncFunction();
+
+    // mariadb.createConnection({
+    //     host: process.env.MYSQL_HOST,
+    //     user: process.env.MYSQL_USER,
+    //     password: process.env.MYSQL_PASSWORD,
+    //     database: process.env.MYSQL_DATABASE
+    // }).then(conn => conn.query("SELECT 1 as val"))
+    //     .then(() => {
+    //         res.json({
+    //             status: 'OK',
+    //             message: 'Service is OK'
+    //         })
+    //     }).catch(() => {
+    //         res.status(500).json({
+    //             status: 'NOT_OK',
+    //             message: 'Service is NOT_OK. Connection to database failed'
+    //         })
+    //     }).finally(() => {
+    //         conn.end()
+    //     })
+    // pool.getConnection()
+    //     .then(connection => {
+    //         res.json({
+    //             status: 'OK',
+    //             message: 'Service is OK'
+    //         })
+    //         connection.end()
+    //     }
+    //     ).catch(e => {
+    //         res.status(500).json({
+    //             status: 'NOT_OK',
+    //             message: 'Service is NOT_OK. Connection to database failed'
+    //         })
+    //     }).finally(() => {
+    //         pool.end()
+    //     });
 })
 app.get('/', (req, res) => {
     res.json({
